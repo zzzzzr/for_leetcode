38e6a980e8a7e4b697ba6d6dfcf0705c9b67ca57
zzzzzr <2647550796@qq.com>
2020-07-20 10:39:28 +0800

    每日一题和顺位做的一题。12题一开始用暴力法做的，后来看了题解，方法更巧妙，这个逻辑可以记一下。

---
+++ Leetcode167.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution167 {
+	public int[] twoSum(int[] numbers, int target) {
+		if(numbers.length<2)
+		{
+			return null;
+		}
+		int i=0;
+		int j=numbers.length-1;
+		while (i<j){
+			if(numbers[i]+numbers[j]<target){
+				i++;
+			}
+			else if(numbers[i]+numbers[j]>target){
+				j--;
+			}
+			else {
+				return new int[]{i+1,j+1};
+			}
+		}
+		return null;
+	}
+	public int[] twoSum_1(int[] numbers, int target) {
+		if(numbers.length<2)
+		{
+			return null;
+		}
+		int []ans=new int[2];
+		//int index1,index2;
+		for(int i=0;i<numbers.length;i++){
+			for(int j=numbers.length-1;j>i;j--){
+				if(numbers[i]+numbers[j]==target){
+					ans[0]=i+1;
+					ans[1]=j+1;
+					return ans;
+				}
+			}
+		}
+		return ans;
+	}
+}
+public class Leetcode167 {
+	public static void main(String []args){
+		int []test=new int []{1,2,5,6};
+		int tar=6;
+		Solution167 sol=new Solution167();
+		System.out.println(Arrays.toString(sol.twoSum(test, tar)) );
+	}
+}
