eb38aaa32fda0b75704f235489b6b112711c426a
zzzzzr <2647550796@qq.com>
2020-08-01 15:30:09 +0800

    增加了一趟遍历的解法

--- Leetcode19.java
+++ Leetcode19.java
@@ -12,7 +12,34 @@ class ListNode {
 	ListNode(int x) { val = x; }
 }
 class Solution19 {
+	// 只用一次遍历实现
+	// 方法：两个指针，间隔n，当后一个到达最后时前一个就在该被删除的位置
 	public ListNode removeNthFromEnd(ListNode head, int n) {
+		// 找出链表的长度
+		int len=0;
+		ListNode ln1=head,ln2=head;
+		// 先让ln2向后走n步，如果不成功则返回null
+		while(n!=0){
+			if(ln2==null)
+				return null;
+			ln2=ln2.next;
+			n--;
+		}
+		// 如果走了n步后ln2为空，则被删除的节点就是第一个节点，则返回head.next
+		if(ln2==null){
+			return head.next;
+		}
+		// 现在ln1,ln2相差的距离和当ln2在最后时ln1在被删除节点前一节点的距离相同，一种保持即可
+		while(ln2.next!=null){
+			ln1=ln1.next;
+			ln2=ln2.next;
+		}
+		//ln2到达最后一个节点，ln1也到达了被删除节点签名的节点，执行删除
+		ln1.next=ln1.next.next;
+		return head;
+	}
+
+	public ListNode removeNthFromEnd_1(ListNode head, int n) {
 		// 找出链表的长度
 		int len=0;
 		ListNode ln=head;
