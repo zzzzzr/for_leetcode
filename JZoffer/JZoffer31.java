934a5981d1de066f510f53eb427770333694008d
zzzzzr <2647550796@qq.com>
2020-08-08 10:00:43 +0800

    继续剑指offer，这是一道贪心算法题目，没有做出来，有空要多看

---
+++ JZoffer/JZoffer31.java
@@ -0,0 +1,31 @@
+package JZoffer;
+
+import java.util.Stack;
+
+/**
+ * 贪心算法
+ * 思路：
+ * 将pushed中的元素不断加入真正的栈中
+ * 每当当前栈顶元素与popped中元素相等时
+ * 弹出栈顶元素，并将指向popped数组的下标向后移
+ * 直到所有元素都加入过栈中
+ * 最后检查栈是否为空，为空则说明有popped描述的pop顺序
+ */
+
+
+class SolutionJZoffer31 {
+	public boolean validateStackSequences(int[] pushed, int[] popped) {
+		Stack<Integer> sta=new Stack<>();
+		int index=0;
+		for (int j : pushed) {
+			sta.push(j);
+			while (sta.peek() == popped[index]) {
+				sta.pop();
+				index++;
+			}
+		}
+		return sta.isEmpty();
+	}
+}
+public class JZoffer31 {
+}
