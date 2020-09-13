10ccddddd652cd794dc93fa5f68557fbb590f3c5
zzzzzr <2647550796@qq.com>
2020-09-13 10:16:20 +0800

    2020/9/12,每日一题

---
+++ Leetcode/Leetcode637.java
@@ -0,0 +1,43 @@
+package Leetcode;
+
+import java.util.*;
+
+//class TreeNode {
+//  int val;
+//  TreeNode left;
+//  TreeNode right;
+//  TreeNode(int x) { val = x; }
+//}
+class Solution637 {
+
+	public List<Double> averageOfLevels(TreeNode root) {
+		List<Double> ans = new LinkedList<>();
+		if (root==null)
+			return ans;
+		Queue<TreeNode> que=new LinkedList<>();
+		que.offer(root);
+		double sum;
+		int num;
+		while (!que.isEmpty()){
+			int len = que.size();
+			sum=0.0;
+			num=0;
+
+			while(num<len&&!que.isEmpty()){
+				TreeNode temp = que.poll();
+				sum+= temp.val;
+				num++;
+				if (temp.left!=null)
+					que.offer(temp.left);
+				if (temp.right!=null)
+					que.offer(temp.right);
+			}
+			ans.add(sum/len);
+		}
+		return ans;
+	}
+
+
+}
+public class Leetcode637 {
+}
