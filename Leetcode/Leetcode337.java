f8bd86d8739795b0c659a954a1f4073b4144f56b
zzzzzr <2647550796@qq.com>
2021-03-15 09:43:27 +0800

    2021/3/15,

--- Leetcode/Leetcode337.java
+++ Leetcode/Leetcode337.java
@@ -4,6 +4,23 @@ import java.util.*;
 import LeetcodeStructure.*;
 //  https://leetcode-cn.com/problems/house-robber-iii/solution/san-chong-fang-fa-jie-jue-shu-xing-dong-tai-gui-hu/
 class Solution337 {
+	public int rob2(TreeNode root) {
+		int []ans =  dfs2(root);
+		return Math.max(ans[0],ans[1]);
+	}
+	public int[] dfs2(TreeNode root){
+		if(root==null)
+			return null;
+		int []left = dfs2(root.left);
+		int []right = dfs2(root.right);
+		if(left==null){
+			return new int[]{right[1]+root.val,Math.max(right[0],right[1])};
+		}
+		if(right==null){
+			return new int[]{left[1]+root.val,Math.max(left[0],left[1])};
+		}
+		return new int[]{left[1]+right[1]+root.val,Math.max(right[0],right[1])+Math.max(left[0],left[1])};
+	}
 
 	public int rob(TreeNode root) {
 		int len=0;
